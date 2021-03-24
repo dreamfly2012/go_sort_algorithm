16f5d191a0a0e93da1e69e79b24a4177394f59f0
menghuiguli <fujia0@qq.com>
2021-03-24 22:05:54 +0800

    merge_sort change

--- merge_sort_test.go
+++ merge_sort_test.go
@@ -7,26 +7,28 @@ import (
 
 func TestMergeSort(t *testing.T) {
 	list := []int{2, 5, 1, 9, 7, 3, 4, 8, 0, 6}
-	MergeSort(list)
-	for i := 0; i < len(list); i++ {
-		fmt.Println(list[i])
+	sortlist := MergeSort(list)
+	for i := 0; i < len(sortlist); i++ {
+		fmt.Println(sortlist[i])
 	}
 	fmt.Println("test merge sort")
 }
 
-func MergeSort(list []int) {
+func MergeSort(list []int) []int {
 	if len(list) < 2 {
-		return
+		return list
 	}
 	//分而治之
 	middle := len(list) / 2
 	left := list[0:middle]
 	right := list[middle:]
-	merge_sort(left, right)
+	left = MergeSort(left)
+	right = MergeSort(right)
+	return merge_sort(left, right)
 
 }
 
-func merge_sort(lista []int, listb []int) {
+func merge_sort(lista []int, listb []int) []int {
 	var result []int
 	for len(lista) != 0 && len(listb) != 0 {
 		if lista[0] < listb[0] {
@@ -46,4 +48,5 @@ func merge_sort(lista []int, listb []int) {
 		result = append(result, listb[0])
 		listb = listb[1:]
 	}
+	return result
 }
